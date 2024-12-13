0fd6d58b76e7aa160c900ab3524c6c037bddd898
Gregorio-Moreta <78994816+Gregorio-Moreta@users.noreply.github.com>
2024-12-13 15:58:42 -0500

    functional react frontend with corresponding express backend with JWT implementations, useContext, routing, fully functional, no bugs

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,29 @@
-const NavBar = ({ user }) => {
+import { useContext } from 'react'; // import useContext to grab the context
+import { AuthedUserContext } from '../../App'; // import the AuthedUserContext
+import { Link } from 'react-router-dom';
+
+const NavBar = ({ handleSignout }) => {
+    const user = useContext(AuthedUserContext); // set the value of user to the imported useContext hook bringing in the value from AuthedUserContext
+
     return (
       <>
-        <p>Hello Navbar</p>
+        { user ? (
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="" onClick={handleSignout}>Sign Out</Link></li>
+            </ul>
+          </nav>
+        ) : (
+          <nav>
+            <ul>
+              <li><Link to="/signin">Sign In</Link></li>
+              <li><Link to="/signup">Sign Up</Link></li>
+            </ul>
+          </nav>
+        )}
       </>
     )
   }
-  export default NavBar
-  
+
+export default NavBar;
